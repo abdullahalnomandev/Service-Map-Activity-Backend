4d4d33c9b2a67faf94c0e87974385c44f441b1c7
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2025-10-08 16:42:18 +0600

    implemented QR code

--- src/app/modules/promo/promo.service.ts
+++ src/app/modules/promo/promo.service.ts
@@ -4,7 +4,7 @@ import QueryBuilder from '../../builder/QueryBuilder';
 import { IPromo } from './promo.interface';
 import { Promo } from './promo.model';
 import mongoose from 'mongoose';
-import { generatePromocode } from './promo.util';
+import { generatePromocode, generateQrCode } from './promo.util';
 
 const createToDB = async (payload: IPromo, userId: mongoose.Types.ObjectId) => {
 
@@ -17,38 +17,60 @@ const createToDB = async (payload: IPromo, userId: mongoose.Types.ObjectId) => {
   if (new Date(payload.endDate).getTime() < new Date(payload.startDate).getTime()) {
     throw new ApiError(StatusCodes.BAD_REQUEST, 'endDate must be after startDate');
   }
-  const generatedPromoCode = await generatePromocode();
 
 
   payload.user = userId;
 
-  if(payload.generatePromocode){
+  if (payload.generatePromocode) {
+    const generatedPromoCode = await generatePromocode();
     payload.promoCode = generatedPromoCode;
   }
 
-  // if(payload.generateQrCode){
-  //   payload.QRCode =  QR; will update later
-  // }
-  
+  if (payload.generateQrCode) {
+    const data = { title: payload.title, descripton: payload.description, discount: payload.discount }
+    const generatedQRCode = await generateQrCode(data, String(userId))
+    payload.QRCode = generatedQRCode
+  }
+
 
   return await Promo.create(payload);
 };
 
-const updateInDB = async ( payload: Partial<IPromo>,userId:mongoose.Types.ObjectId) => {
+const updateInDB = async (payload: Partial<IPromo>, userId: mongoose.Types.ObjectId) => {
   if (payload.startDate && payload.endDate) {
     if (new Date(payload.endDate).getTime() < new Date(payload.startDate).getTime()) {
       throw new ApiError(StatusCodes.BAD_REQUEST, 'endDate must be after startDate');
     }
   }
 
-  const updated = await Promo.findOneAndUpdate({user:userId}, payload, { new: true });
-  if (!updated) {
-    throw new ApiError(StatusCodes.NOT_FOUND, 'Promo not found');
+  if (payload.generatePromocode) {
+    const generatedPromoCode = await generatePromocode();
+    payload.promoCode = generatedPromoCode;
+  } else {
+    payload.promoCode = '';
   }
+
+  if (payload.generateQrCode) {
+    const data = { title: payload.title, descripton: payload.description, discount: payload.discount };
+    const generatedQRCode = await generateQrCode(data, String(userId));
+    payload.QRCode = generatedQRCode;
+  } else {
+    payload.QRCode = '';
+  }
+
+  console.log(payload)
+
+  const updated = await Promo.findOneAndUpdate(
+    { user: userId },
+    { $set: payload, $setOnInsert: { user: userId } },
+    { new: true, upsert: true }
+  );
+
   return updated;
 };
 
-const deleteFromDB = async (userId:mongoose.Types.ObjectId) => {
+
+const deleteFromDB = async (userId: mongoose.Types.ObjectId) => {
   const deleted = await Promo.findOneAndDelete(userId);
   if (!deleted) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Promo not found');
@@ -69,10 +91,10 @@ const getAllFromDB = async (query: Record<string, any>) => {
 };
 
 const getByIdFromDB = async (id: mongoose.Types.ObjectId) => {
-  
-   console.log({id})
 
-  const doc = await Promo.findOne({user:id}).populate('user', 'name email profileImage').lean();
+  console.log({ id })
+
+  const doc = await Promo.findOne({ user: id }).populate('user', 'name email profileImage').lean();
   if (!doc) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Promo not found');
   }
